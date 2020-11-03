0d5f9aa77dd8e1c8ab0d74fec1d6024c49e85619
Matthew Caldwell <caldwellysr@gmail.com>
2020-11-02 21:55:27 -0600

    Create a loading scene with progress bar

--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -1,32 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using Moonshot.UI;
 
 namespace Moonshot.GameManagement {
 
 	public class GameManager : MonoBehaviour {
 		#region Singleton
-		private static GameManager instance = null;
-		private static readonly object padlock = new object();
+		public static GameManager Instance { get; private set; }
 
-		GameManager() { }
-
-		public static GameManager Instance {
-			get {
-				lock (padlock) {
-					if (instance == null) {
-						instance = new GameManager();
-					}
-					return instance;
-				}
+		void Awake() {
+			if (Instance == null) {
+				Instance = this;
+				DontDestroyOnLoad(gameObject);
+			} else {
+				DestroyImmediate(gameObject);
+				return;
 			}
 		}
 		#endregion
 
+		[SerializeField] private GameObject LoadingScreen;
+		[SerializeField] private ProgressBar m_ProgressBar;
+
+		List<AsyncOperation> scenesLoading = new List<AsyncOperation>();
+		float TotalProgress;
+
 		#region Unity Methods
-		private void Awake() {
-			SceneManager.LoadSceneAsync((int)SceneIndexes.TITLE_SCENE, LoadSceneMode.Additive);
+		private void Start() {
+			SceneManager.LoadSceneAsync((int)SceneIndexes.TITLE_SCREEN, LoadSceneMode.Additive);
 		}
 		#endregion
+
+		#region Custom Methods
+		public void LoadGame() {
+			LoadingScreen.SetActive(true);
+
+			scenesLoading.Add(SceneManager.UnloadSceneAsync((int)SceneIndexes.TITLE_SCREEN));
+			scenesLoading.Add(SceneManager.LoadSceneAsync((int)SceneIndexes.MAIN, LoadSceneMode.Additive));
+
+			StartCoroutine(GetSceneLoadProgress());
+		}
+
+		public IEnumerator GetSceneLoadProgress() {
+			for (int i = 0; i < scenesLoading.Count; i++) {
+				while (!scenesLoading[i].isDone) {
+					TotalProgress = 0;
+
+					foreach (AsyncOperation operation in scenesLoading) {
+						TotalProgress += operation.progress;
+					}
+
+					TotalProgress = (TotalProgress / scenesLoading.Count) * 100f;
+
+					m_ProgressBar.Current = Mathf.RoundToInt(TotalProgress);
+					yield return null;
+				}
+			}
+
+			LoadingScreen.SetActive(false);
+		}
+		#endregion
+
 	}
 
 }
---
+++ Assets/Scripts/UI/LoadGameButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Moonshot.GameManagement;
+
+namespace Moonshot.UI { 
+
+	public class LoadGameButton : MonoBehaviour {
+
+		public void LoadGame() {
+			GameManager.Instance.LoadGame();
+		}
+
+	}
+
+}
