432797db1df8836b27bf84669f182809411cb5c0
Matthew Caldwell <caldwellysr@gmail.com>
2020-11-04 16:30:03 -0600

    Add Idle/Walk Blend Trees and Animation Controller

---
+++ Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Moonshot.Player {
+
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(BaseInput))]
+	public class AnimationController : MonoBehaviour {
+
+		private BaseInput input;
+		private Animator anim;
+
+		private void Awake() {
+			input = GetComponent<BaseInput>();
+			anim = GetComponent<Animator>();
+		}
+
+		void Update() {
+			Vector2 move = new Vector2(input.Horizontal, input.Vertical);
+			anim.SetBool("Moving", move.magnitude != 0f);
+
+			if (move == Vector2.zero) {
+				return; 
+			}
+
+			anim.SetFloat("MoveX", move.x);
+			anim.SetFloat("MoveY", move.y);
+		}
+	}
+}
