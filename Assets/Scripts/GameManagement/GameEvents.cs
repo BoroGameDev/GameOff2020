7aa7b74cc7484e8c9b35a1c6b095ed97f3ff611c
Matthew Caldwell <caldwellysr@gmail.com>
2020-11-05 07:10:51 -0600

    Add initial cutscene and singleton for managing cutscenes

--- Assets/Scripts/GameManagement/GameEvents.cs
+++ Assets/Scripts/GameManagement/GameEvents.cs
@@ -2,37 +2,44 @@
 
 namespace Moonshot.GameManagement {
 
-	public class GameEvents {
-		#region Singleton
-		private static GameEvents instance = null;
-		private static readonly object padlock = new object();
+  public class GameEvents {
+    #region Singleton
+    private static GameEvents instance = null;
+    private static readonly object padlock = new object();
 
-		GameEvents() { }
+    GameEvents() { }
 
-		public static GameEvents Instance {
-			get {
-				lock (padlock) {
-					if (instance == null) {
-						instance = new GameEvents();
-					}
-					return instance;
-				}
-			}
-		}
-		#endregion
+    public static GameEvents Instance {
+      get {
+        lock (padlock) {
+          if (instance == null) {
+            instance = new GameEvents();
+          }
+          return instance;
+        }
+      }
+    }
+    #endregion
 
-		public event Action onDialogueStarted;
-		public void DialogueStarted() {
-			if (onDialogueStarted != null) {
-				onDialogueStarted();
-			}
-		}
+    public event Action onDialogueStarted;
+    public void DialogueStarted() {
+      if (onDialogueStarted != null) {
+        onDialogueStarted();
+      }
+    }
 
-		public event Action onDialogueEnded;
-		public void DialogueEnded() {
-			if (onDialogueEnded != null) {
-				onDialogueEnded();
-			}
-		}
-	}
+    public event Action onDialogueEnded;
+    public void DialogueEnded() {
+      if (onDialogueEnded != null) {
+        onDialogueEnded();
+      }
+    }
+
+    public event Action<SceneIndexes> onSceneLoaded;
+    public void SceneLoaded(SceneIndexes _index) {
+      if (onSceneLoaded != null) {
+        onSceneLoaded(_index);
+      }
+    }
+  }
 }
