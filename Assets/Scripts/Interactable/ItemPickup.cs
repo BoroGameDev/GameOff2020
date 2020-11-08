5f8c2627b54c5c22fa3e4ea43a417c9a5a3bd14d
Matthew Caldwell <caldwellysr@gmail.com>
2020-11-08 07:28:20 -0600

    Add pickupable items

---
+++ Assets/Scripts/Interactable/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using Moonshot.Inventories;
+using Moonshot.Items;
+
+using UnityEngine;
+
+namespace Moonshot.Interactables {
+	public class ItemPickup : Interactable {
+		public Item item;
+
+		public override void Interact() {
+			base.Interact();
+
+			Pickup();
+		}
+
+		private void Pickup() {
+			Debug.Log($"Picking up {item.name}");
+
+			Inventory.Instance.Add(item);
+
+			Destroy(gameObject);
+		}
+	}
+}
