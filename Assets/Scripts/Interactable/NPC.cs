30610bdb98e1a17fcd7998a90dd7af27fb2a5f27
Neil Spackman <n.spackman89@gmail.com>
2020-11-09 21:40:20 -0600

    feat: integrated NPC with dialogue, added UI events for dialogue

--- Assets/Scripts/Interactable/NPC.cs
+++ Assets/Scripts/Interactable/NPC.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Moonshot.GameManagement;
 using UnityEngine;
 
 public class NPC : Interactable {
 
 	private string[] dialogues = { "Hello.", "Goodbye.", "I am an NPC!" };
+	public Dialogue dialogue;
 	public override void Interact() {
 		/*
-            TODO:
-            - check if quest in progress
-            - if yes
-                - either use reminder of quest or use default dialogue
-            - if not
-                - get related quest (If multiple, grab the first)
-                - check if can do quest (Existing dependencies)
-                - init quest dialog
-            - 
-        */
-		Respond();
+			TODO:
+			- check if quest in progress
+			- if yes
+				- either use reminder of quest or use default dialogue
+			- if not
+				- get related quest (If multiple, grab the first)
+				- check if can do quest (Existing dependencies)
+				- init quest dialog
+			- 
+		*/
+		// FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		DialogueManager.Instance.StartDialogue(dialogue);
+		GameEvents.Instance.DialogueStarted();
+		// Respond();
 	}
 
 	public virtual void Respond(string message = "") {
