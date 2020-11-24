63325735e343d70be1dbf9368c02179cba0e2dab
Neil Spackman <n.spackman89@gmail.com>
2020-11-23 20:19:26 -0600

    added ui for inventory

---
+++ Assets/InventoryItemSlot.cs
@@ -0,0 +1,17 @@
+﻿using Moonshot.Items;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class InventoryItemSlot : MonoBehaviour {
+	private Image itemSprite;
+	public Item item;
+
+	private void Awake() {
+		itemSprite = GetComponent<Image>();
+	}
+
+	public void Draw() {
+		itemSprite.sprite = item.icon;
+	}
+}
---
+++ Assets/InventorySection.cs
@@ -0,0 +1,32 @@
+﻿using Moonshot.GameManagement;
+using Moonshot.Inventories;
+using UnityEngine;
+
+public class InventorySection : MonoBehaviour {
+
+	[SerializeField] private GameObject itemSlotPrefab;
+
+	void Start() {
+		GameEvents.Instance.onInventoryUpdated += DrawInventory;
+	}
+
+	void OnDestroy() {
+		GameEvents.Instance.onInventoryUpdated -= DrawInventory;
+	}
+
+	private void DrawInventory() {
+		DesimateChildren();
+		foreach (var item in Inventory.Instance.items) {
+			GameObject itemSlot = Instantiate(itemSlotPrefab);
+			itemSlot.transform.parent = this.transform;
+			itemSlot.GetComponent<InventoryItemSlot>().item = item;
+			itemSlot.GetComponent<InventoryItemSlot>().Draw();
+		}
+	}
+
+	private void DesimateChildren() {
+		foreach (Transform child in transform) {
+			Destroy(child.gameObject);
+		}
+	}
+}
